22052b7bc9ab40bb1da28a42e19c0230274970ac
imnider <velezzambranoneider@gmail.com>
2026-04-15 20:30:43 -0500

    feat: agregar cache, hasher y generador de cadenas de texto

---
+++ backend/with-database/YoutubeClone/YoutubeClone.Shared/Generate.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace YoutubeClone.Shared
+{
+    public class Generate
+    {
+        private const string Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        public static string RandomText(int length = 50)
+        {
+            var value = new StringBuilder();
+            var rnd = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                value.Append(Characters[rnd.Next(0, Characters.Length - 1)]);
+            }
+
+            return value.ToString();
+        }
+    }
+}
