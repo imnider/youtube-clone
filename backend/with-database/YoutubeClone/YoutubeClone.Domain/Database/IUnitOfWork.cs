7430d8583d2a365144eb5b1a98d35a7ba7f66754
imnider <velezzambranoneider@gmail.com>
2026-04-15 22:52:04 -0500

    feat: agregar UnitOfWork

---
+++ backend/with-database/YoutubeClone/YoutubeClone.Domain/Database/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using YoutubeClone.Domain.Interfaces.Repositories;
+
+namespace YoutubeClone.Domain.Database
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository _userRepository { get; set; }
+        Task SaveChangesAsync();
+    }
+}
---
+++ backend/with-database/YoutubeClone/YoutubeClone.Infrastructure/Persistence/SqlServer/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using YoutubeClone.Domain.Database;
+using YoutubeClone.Domain.Database.SqlServer.Context;
+using YoutubeClone.Domain.Interfaces.Repositories;
+
+namespace YoutubeClone.Infrastructure.Persistence.SqlServer
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly YoutubeCloneContext _context;
+        public IUserRepository _userRepository { get; set; }
+
+        public UnitOfWork(YoutubeCloneContext context, IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            _context = context;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+}
