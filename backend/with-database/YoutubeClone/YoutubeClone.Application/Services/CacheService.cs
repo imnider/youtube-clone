d226cb84009f43fd487962c466a421217ae5877e
imnider <velezzambranoneider@gmail.com>
2026-04-15 20:58:19 -0500

    feat: implementar servicio de caché

---
+++ backend/with-database/YoutubeClone/YoutubeClone.Application/Services/CacheService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using YoutubeClone.Application.Interfaces.Services;
+
+namespace YoutubeClone.Application.Services
+{
+    public class CacheService(IMemoryCache memoryCache) : ICacheService
+    {
+        public T Create<T>(string key, TimeSpan expiration, T value)
+        {
+            try
+            {
+                var create = memoryCache.GetOrCreate(key, (factory) =>
+                {
+                    factory.SlidingExpiration = expiration;
+                    return value;
+                });
+                return create is null ? throw new Exception("No se pudo establecer la caché") : create;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(string key)
+        {
+            try
+            {
+                memoryCache.Remove(key);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public T? Get<T>(string key)
+        {
+            try
+            {
+                return memoryCache.Get<T>(key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
