910c8a7923f557cef07636e0568095cfc4c295a4
imnider <velezzambranoneider@gmail.com>
2026-04-15 20:45:52 -0500

    feat: agregar modelo de request pata Auth

---
+++ backend/with-database/YoutubeClone/YoutubeClone.Application/Models/Requests/Auth/LoginAuthRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YoutubeClone.Shared.Constants;
+
+namespace YoutubeClone.Application.Models.Requests.Auth
+{
+    public class LoginAuthRequest
+    {
+        [Required(ErrorMessage = ValidationConstants.REQUIRED)]
+        [EmailAddress(ErrorMessage = ValidationConstants.EMAIL)]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = ValidationConstants.REQUIRED)]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ backend/with-database/YoutubeClone/YoutubeClone.Application/Models/Requests/Auth/RenewAuthRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YoutubeClone.Application.Models.Requests.Auth
+{
+    public class RenewAuthRequest
+    {
+        [Required]
+        public string RefreshToken { get; set; } = null!;
+    }
+}
