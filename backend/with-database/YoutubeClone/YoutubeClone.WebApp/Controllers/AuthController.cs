c148d7e6ffe778625cfb115b6a9846be96cd2590
imnider <velezzambranoneider@gmail.com>
2026-04-15 21:14:55 -0500

    feat: agregar controlador para Auth

---
+++ backend/with-database/YoutubeClone/YoutubeClone.WebApp/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YoutubeClone.Application.Interfaces.Services;
+using YoutubeClone.Application.Models.Requests.Auth;
+
+namespace YoutubeClone.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthServices service) : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginAuthRequest model)
+        {
+            var srv = await service.Login(model);
+            return Ok(srv);
+        }
+
+        [HttpPost("renew")]
+        public async Task<IActionResult> Renew([FromBody] RenewAuthRequest model)
+        {
+            var srv = await service.Renew(model);
+            return Ok(srv);
+        }
+    }
+}
