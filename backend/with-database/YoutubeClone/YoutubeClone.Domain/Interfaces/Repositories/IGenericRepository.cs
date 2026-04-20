5a3872f3ac9694159492f441006ab404db655c2a
imnider <velezzambranoneider@gmail.com>
2026-04-19 22:36:22 -0500

    feat: agregar repositorio genérico

---
+++ backend/with-database/YoutubeClone/YoutubeClone.Domain/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace YoutubeClone.Domain.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Create(T entity);
+        Task<T> Update(T entity);
+        Task<bool> IfExists(Expression<Func<T, bool>> expression);
+        Task<T?> Get(Expression<Func<T, bool>> expression);
+        IQueryable<T> Queryable();
+        Task<bool> Delete(T entity);
+    }
+}
---
+++ backend/with-database/YoutubeClone/YoutubeClone.Infrastructure/Persistence/SqlServer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using YoutubeClone.Domain.Database.SqlServer.Context;
+
+namespace YoutubeClone.Infrastructure.Persistence.SqlServer.Repositories
+{
+    public class GenericRepository<T>(YoutubeCloneContext context) where T : class
+    {
+        public async Task<T> Create(T entity)
+        {
+            context.Set<T>().Add(entity);
+            return entity;
+        }
+
+        public async Task<bool> Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            return true;
+        }
+
+        public IQueryable<T> Queryable()
+        {
+            return context.Set<T>().AsQueryable();
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            context.Set<T>().Update(entity);
+            return entity;
+        }
+
+        public async Task<bool> IfExists(Expression<Func<T, bool>> expression)
+        {
+            return await context.Set<T>().AnyAsync(expression);
+        }
+
+        public async Task<T?> Get(Expression<Func<T, bool>> expression)
+        {
+            return await context.Set<T>().FirstOrDefaultAsync(expression);
+        }
+    }
+}
